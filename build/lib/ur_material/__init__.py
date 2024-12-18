39ee5d53e2361172979940a363845c3f2bb3c420
caoboqian <bcao4832@uni.sydney.edu.au>
2024-12-18 16:16:53 +0800

    deleted useless files and dirs

--- build/lib/ur_material/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import large
-from . import small
--- build/lib/ur_material/small/__init__.py
+++
@@ -1,2 +0,0 @@
-from .functions import ink_on_paper, ink_on_paper_light, ink_bleed, low_quality\
-, erosed_light, erosed_heavy, low_quality_on_paper, noised, cutted, erosed_cutted
