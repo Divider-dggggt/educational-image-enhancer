a298d1edadb953abc238faf5ec80ccff256a23e8
caoboqian <bcao4832@uni.sydney.edu.au>
2024-12-18 13:22:29 +0800

    reformed_structure

---
+++ build/lib/ur_material/large/functions.py
@@ -0,0 +1,57 @@
+import src.ur_material.large.image_enhance_large as image_enhance_large
+
+def blur_watermarks(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def shadow_on_paper(image):
+
+    return image_enhance_large.image_augmentation_1(image)
+
+def ink_around_blur(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def ink_bleed_blur(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def blur_transform(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def reduce_ink(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def ink_around_twisted(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def old_papers(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def twisted_erosed(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def low_quality_twisted(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def erosed_low_quality(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def random_impact(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def scanned_ink(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def twisted_paper(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
---
+++ src/ur_material/large/functions.py
@@ -0,0 +1,58 @@
+import image_enhance_large
+import cv2
+
+def blur_watermarks(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def shadow_on_paper(image):
+
+    return image_enhance_large.image_augmentation_1(image)
+
+def ink_around_blur(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def ink_bleed_blur(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def blur_transform(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def reduce_ink(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def ink_around_twisted(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def old_papers(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def twisted_erosed(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def low_quality_twisted(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def erosed_low_quality(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def random_impact(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def scanned_ink(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
+
+def twisted_paper(image):
+    
+    return image_enhance_large.image_augmentation_1(image)
